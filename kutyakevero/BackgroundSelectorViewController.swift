e773cf601ca67d4b3007ab14d6e17fdbe1c77040
Peter Adam Korodi <pkorodi@logmein.com>
2017-12-19 10:55:09 +0100

    backgroundScreen

--- kutyakevero/BackgroundSelectorViewController.swift
+++ kutyakevero/BackgroundSelectorViewController.swift
@@ -7,9 +7,28 @@
 
 import UIKit
 
-class BackgroundSelectorViewController: UIViewController {
+typealias Point = (Int, Int)
+struct BackgroundItem {
+    let imageName: String
+    let dogTranslate: Point
+    let dogScale: Double
+    let dogRotation: Double
+}
 
+class BackgroundSelectorViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {    
+
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var nextButton: UIBarButtonItem!
+    
+    private var backgrounds: [BackgroundItem] = [
+        BackgroundItem(imageName: "background1", dogTranslate: (0, 0), dogScale: 1, dogRotation: 0),
+        BackgroundItem(imageName: "background4", dogTranslate: (0, 0), dogScale: 1, dogRotation: 0),
+        BackgroundItem(imageName: "background5", dogTranslate: (0, 0), dogScale: 1, dogRotation: 0),
+        BackgroundItem(imageName: "background6", dogTranslate: (0, 0), dogScale: 1, dogRotation: 0),
+        BackgroundItem(imageName: "background7", dogTranslate: (0, 0), dogScale: 1, dogRotation: 0),
+        BackgroundItem(imageName: "background8", dogTranslate: (0, 0), dogScale: 1, dogRotation: 0),
+        BackgroundItem(imageName: "background9", dogTranslate: (0, 0), dogScale: 1, dogRotation: 0),
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +36,16 @@ class BackgroundSelectorViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return backgrounds.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCollectionViewCell", for: indexPath) as! PhotoCellCollectionViewCell
+        cell.imageView.image = UIImage(named: backgrounds[indexPath.item].imageName)
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ kutyakevero/PhotoCellCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCellCollectionViewCell.swift
+//  kutyakevero
+//
+//  Created by Peter Adam Korodi on 2017. 12. 18..
+//  Copyright © 2017. Adam Horvath. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
